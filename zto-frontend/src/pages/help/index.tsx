ef09515c48afa7fa8a085fee1e83aabcbb454f34
BMunkhtur <azuremunkhtur@gmail.com>
2023-04-25 12:35:09 +0800

    minFooter push

--- zto-frontend/src/pages/help/index.tsx
+++ zto-frontend/src/pages/help/index.tsx
@@ -1,9 +1,11 @@
+import MinFooter from '@/components/MinFooter';
 import React from 'react';
 import HelpPage from "../../components/Help"
 
 const Help = () => {
   return (<>
    <HelpPage/>
+   <MinFooter/>
   </>
   )
 }
