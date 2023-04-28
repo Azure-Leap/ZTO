9e00c04dbcada7710b5390d90282045b8715ea65
Enkhtvshn <Enkhtuwshin.1201@gmail.com>
2023-04-28 18:31:26 +0800

    Category-filt

---
+++ zto-frontend/src/pages/website-templates/[categoryId].tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import Category from "../../components/Categories";
+import { useRouter } from "next/router";
+
+const WebsiteTemplates = () => {
+  const router: any = useRouter();
+
+  console.log("RR", router.query);
+
+  // useEffect();//getCatgory
+  // axios.get("categpory/categoryId"); => []
+  // data={catgory}
+  return (
+    <>
+      <Category />
+    </>
+  );
+};
+
+export async function getServerSideProps({ query }: any) {
+  const res = await fetch(`http://localhost:9010/categories/${query.id}`);
+  const data = await res.json();
+
+  return {
+    props: { category: data.data },
+  };
+}
+
+export default WebsiteTemplates;
