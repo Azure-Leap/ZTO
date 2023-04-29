4247d41c9c75c5400fad11e9120d06c38fa4fbcb
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2023-04-29 16:04:28 +0800

    editPage

---
+++ zto-frontend/src/pages/editPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TemplateHome from './onepirate/TemplateHome'
+
+const EditPage = () => {
+  return (
+    <div>
+        <TemplateHome/>
+    </div>
+  )
+}
+
+export default EditPage
