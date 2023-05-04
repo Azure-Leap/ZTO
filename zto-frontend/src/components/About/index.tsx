36620c36375e4afbe21b5d684d224c2705f1ea5b
BMunkhtur <azuremunkhtur@gmail.com>
2023-05-04 14:06:15 +0800

    One-page

--- zto-frontend/src/components/About/index.tsx
+++ zto-frontend/src/components/About/index.tsx
@@ -26,7 +26,7 @@ const catType = [
 ];
 const About = () => {
   return (
-    <Grid ><Grid sx={{display:"flex",  justifyContent:"space-between", margin:"240px", position:"relative"}}>
+    <Grid ><Grid sx={{display:"flex",  justifyContent:"space-between", margin:"240px"}}>
     <Grid>
               <Typography variant='h2' sx={{position:"sticky", top:"200px"}} className=''>Бидний тухай</Typography></Grid>
       <Grid  sx={{display:"flex", flexDirection:"column", gap:"90px"}}>{catType.map((card) => (
---
+++ zto-frontend/src/pages/website-templates/Search.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Paper } from '@mui/material'
+import {IconButton} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+
+const Search = () => {
+  return (
+    <>
+    <Paper sx={{ p: '2px 4px', display: 'flex', flexDirection:"row", alignItems: 'center', width: "100%" , marginLeft:"20%"}}>
+      <IconButton sx={{ p: '10px' }}>
+        <MenuIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search zto template"
+        inputProps={{ 'aria-label': 'search zto template' }}
+      />
+      <IconButton  color="primary" type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+    </Paper>
+    </>
+  )
+}
+export default Search
