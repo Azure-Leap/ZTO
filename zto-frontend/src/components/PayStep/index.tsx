3c2fb4bca077d744152c531eb76f4a739e029676
Evening <evening@mstars-lab3-01s-iMac.local>
2023-05-19 18:45:20 +0800

    jijig aldaa zasaw

--- zto-frontend/src/components/PayStep/index.tsx
+++ zto-frontend/src/components/PayStep/index.tsx
@@ -2,15 +2,17 @@ import { Stepper, Step, StepLabel, Button, Box } from "@mui/material";
 
 export default function App({ activeStep, nextStep, prevStep }: any): any {
   return (
-    <Box  sx={{    
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    maxHeight: "40vh",
-    minHeight: "38vh",
-    borderRadius: "15px",}}
-       >
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        maxHeight: "40vh",
+        minHeight: "38vh",
+        borderRadius: "15px",
+      }}
+    >
       {/* <Stepper
         activeStep={activeStep}
         sx={{ background: "white", borderRadius: "20px", padding: 2 }}
@@ -18,35 +20,35 @@ export default function App({ activeStep, nextStep, prevStep }: any): any {
        
       </Stepper> */}
       <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-        <Button    sx={{
-              color: "black",
-              border: "1px solid white",
-              padding: 2,
-              borderRadius: "10px",
-              backgroundColor:"blue",
-              width:"30%",
-              opacity:"0.7"
-            }} onClick={() => prevStep()}>
-          <Box
-         
-          >
-            VISA
-          </Box>
+        <Button
+          sx={{
+            color: "white",
+            border: "1px solid white",
+            padding: 2,
+            borderRadius: "10px",
+            backgroundColor: "blue",
+            width: "30%",
+            opacity: "0.7",
+            ":hover": { color: "black" },
+          }}
+          onClick={() => prevStep()}
+        >
+          <Box>VISA</Box>
         </Button>
-        <Button   sx={{
-              color: "black",
-              border: "1px solid white",
-              padding: 2,
-              borderRadius: "10px",
-              backgroundColor:"blue",
-              width:"30%",
-              opacity:"0.7"
-            }} onClick={() => nextStep()}>
-          <Box
-          
-          >
-            QR
-          </Box>
+        <Button
+          sx={{
+            color: "white",
+            border: "1px solid white",
+            padding: 2,
+            borderRadius: "10px",
+            backgroundColor: "blue",
+            width: "30%",
+            opacity: "0.7",
+            ":hover": { color: "black" },
+          }}
+          onClick={() => nextStep()}
+        >
+          <Box>QR</Box>
         </Button>
       </Box>
     </Box>
